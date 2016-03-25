3ec580afb67382bc94a97dfb5ed8fa4080653a60
Michael A Griffey <mikeagriffey@uncannysoft.com>
2016-03-25 13:46:48 -0500

    Initial project setup

---
+++ UncannySoft.Authorization/src/UncannySoft.MvcAuthExample/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNet.Mvc;
+
+namespace UncannySoft.MvcAuthExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index() => View();
+
+        [Route("Error")]
+        public IActionResult Error() => View();
+    }
+}
---
+++ UncannySoft.Authorization/src/UncannySoft.MvcAuthExample/Startup.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace UncannySoft.MvcAuthExample
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .AddJsonFile("authsettings.json")
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+        public IConfigurationRoot Configuration { get; set; }
+        
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+        
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+
+            app.UseDeveloperExceptionPage();
+            app.UseIISPlatformHandler();
+            app.UseStaticFiles();
+
+            app.UseMvcWithDefaultRoute();
+        }
+        
+        public static void Main(string[] args) => WebApplication.Run<Startup>(args);
+    }
+}
